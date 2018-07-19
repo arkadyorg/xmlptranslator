98e04121fbbf85f2b1217b6d9f3af4d92e064f30
Arkady Bocharov <me@arkady.org>
2018-07-20 00:17:24 +0300

    formated data

--- filelogic.py
+++ filelogic.py
@@ -1,10 +1,11 @@
 import xml.etree.ElementTree as ET  
 tree = ET.parse('0201 Birth dates.xdo')
 root = tree.getroot()
-
+'''
 for item in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}parameters//'):
-    print(item.tag, item.keys(), item.items(), item.text)
+    print('ITEM TAG:: {}, ITEM KEYS:: {} ITEM ITEMS:: {} ITEM TEXT:: {} '.format(item.tag, item.keys(), item.items(), item.text))
 
-
+'''
 for item in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}templates//'):
-    print(item.tag, item.keys(), item.items(), item.text)
+    print('ITEM TAG:: {}, ITEM KEYS:: {} ITEM ITEMS:: {} ITEM TEXT:: {} '.format(item.tag, item.keys(), item.items(), item.text))
+    
