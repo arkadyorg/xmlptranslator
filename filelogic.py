9a1c53cdad14dba2b9b70b38ee29d10f6420eddc
Arkady Bocharov <me@arkady.org>
2018-07-21 23:20:24 +0300

    get all templates by filename

--- filelogic.py
+++ filelogic.py
@@ -5,7 +5,11 @@ root = tree.getroot()
 for item in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}parameters//'):
     print('ITEM TAG:: {}, ITEM KEYS:: {} ITEM ITEMS:: {} ITEM TEXT:: {} '.format(item.tag, item.keys(), item.items(), item.text))
 
-'''
+
 for item in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}templates//'):
-    print('ITEM TAG:: {}, ITEM KEYS:: {} ITEM ITEMS:: {} ITEM TEXT:: {} '.format(item.tag, item.keys(), item.items(), item.text))
-    
+    print('ITEM ITEMS: {}'.format(item.items()))
+'''
+#def templates_list(filename):
+for template_label in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}templates//'):
+	label = template_label.attrib
+	print (label['label'])
