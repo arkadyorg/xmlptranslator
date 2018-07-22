493a21b0283cf89ff07cb02ecc8b92ac44f8b0c5
Arkady Bocharov <me@arkady.org>
2018-07-22 22:53:29 +0300

    added all db tables base configuration for report initial parameters get

--- db.py
+++ db.py
@@ -14,7 +14,7 @@ class Reports(Base):
     report_name = Column(String(100))
     report_dir = Column(String(300))
     file_name = Column(String(100))
-    base_template = Column(String(100))
+    default_template = Column(String(100))
     base_lang = Column(String(100))
     created = Column(TIMESTAMP, nullable=False)
     updated = Column(TIMESTAMP, nullable=False)
@@ -33,6 +33,30 @@ class Languages(Base):
 	def __repr__(self):
 		return '<Language space {} was created {} and updated {}>'.format(self.locale, self.created, self.updated)
 
+class Templates(Base):
+	__tablename__ = 'templates'
+	id = Column(Integer, primary_key=True)
+	report_id = Column(Integer)
+	template_label = Column(String(100))
+	template_type = Column(String(100))
+	template_url = Column(String(100))
+	template_lang = Column(String(100))
+	created = Column(TIMESTAMP, nullable=False)
+	updated = Column(TIMESTAMP, nullable=False)
+
+	def __repr__(self):
+		return '<Template {} for {}>'.format(self.template_label, self.report_id)
+
+class Parameters(Base):
+	__tablename__ = 'parameters_list'
+	id = Column(Integer, primary_key=True)
+	report_id = Column(Integer)
+	base_label = Column(String(100))
+	created = Column(TIMESTAMP, nullable=False)
+	updated = Column(TIMESTAMP, nullable=False)
+
+	def __repr__(self):
+		return '<Parameters list for report {}>'.format(self.report_id)
 
 
 if __name__ == "__main__":
