0f55eeda46c9524bc20d1e51dc7df3dde84e5260
Arkady Bocharov <me@arkady.org>
2018-07-25 21:49:11 +0300

    fixed reports naming procedure due to inconsistent title naming in xdo files

--- filelogic.py
+++ filelogic.py
@@ -1,5 +1,4 @@
 import os, fnmatch, collections 
-from xmlgetter import report_title
 from dblogic import report_reindex
 
 
@@ -8,7 +7,7 @@ exlist = ['*.xdo']
 for root, directories, filenames in os.walk('./original/'):
 	for extensions in exlist:
 		for filename in fnmatch.filter(filenames, extensions):
-			title = report_title(os.path.join(root, filename))
+			title = (os.path.join(filename)).replace(".xdo", "")
 			report_directory = (os.path.join(root))
 			file_name = (os.path.join(filename))
 			report_reindex(title, report_directory, file_name)
