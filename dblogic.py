67340a97bb8488f3cf1c3a7a10384540be626dd7
Arkady Bocharov <me@arkady.org>
2018-07-25 21:20:41 +0300

    added checker if report already exists in reports db

--- dblogic.py
+++ dblogic.py
@@ -3,6 +3,10 @@ from datetime import datetime
 
 
 def report_reindex(r_name,r_dir,r_file):
-	report_item=Reports(report_name=r_name, report_dir=r_dir, file_name=r_file, created=datetime.now(), updated=datetime.now())
-	db_session.add(report_item)
-	db_session.commit()
+	exists = db_session.query(Reports.report_name).filter_by(report_name=r_name).scalar() is not None
+	if exists == False:
+		report_item=Reports(report_name=r_name, report_dir=r_dir, file_name=r_file, created=datetime.now(), updated=datetime.now())
+		db_session.add(report_item)
+		db_session.commit()
+	else:
+		pass
