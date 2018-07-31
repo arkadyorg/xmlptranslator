3e01e7391fd07141dcf2dccfd1013b15375ea029
Arkady Bocharov <me@arkady.org>
2018-08-01 00:09:55 +0300

    xmlgetter added parameters_lister function

--- xmlgetter.py
+++ xmlgetter.py
@@ -35,3 +35,25 @@ def template_lister(rep_id, file_pointer):
 		template_item = {'td_entry_num':i, 'td_report_id':rep_id, 'td_template_label': label.get('label'),'td_template_type':label.get('type'),'td_template_url':label.get('url'), 'td_template_lang': p.upper() }
 		template_data.append(template_item)
 	return template_data
+
+def parameters_lister(rep_id, file_pointer):
+	tree = ET.parse(file_pointer)
+	root = tree.getroot()
+	i = 0
+	param_list = []
+	for param in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}parameters//'):
+		label = param.attrib
+		id = param.attrib
+		param_lable=''
+		if 'id' in id:
+			param_id = id.get('id')
+		elif 'label' in label:
+			param_lable = label.get('label')
+			i+=1
+			param_item = {'pl_entry_num':i,'pl_report_id': rep_id, 'pl_param_id':param_id, 'pl_param_lable':param_lable}
+			param_list.append(param_item)
+		else:
+			pass
+	return param_list
+
+
