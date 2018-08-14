2e706360f4d5c394470934b647634214852e549f
Arkady Bocharov <me@arkady.org>
2018-08-14 22:34:11 +0300

    added show DB data as html

--- server.py
+++ server.py
@@ -5,15 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	'''
 	result = language_list()
-	names = ''
-	for langname in result:
-		names = names + '<p><a href="/language/' + langname['code'] + '">'+ langname['name']  +'</a></p>'
-	return str(names)
-	'''
-	return render_template('index.html')
-
+	return render_template('index.html', languages=result)
 
 @app.route("/config")
 def config():
@@ -26,10 +19,13 @@ def about():
 @app.route("/language/<lang_code>")
 def reports_by_lang(lang_code):
 	result = report_list()
+	
 	names = ''
 	for repname in result:
 		names = names + '<p>' + repname['name'] + '</p>'
 	return str(names)
+	
+	#return render_template('index.html', languages=result)
 
 if __name__ == "__main__":
 	app.run(port=1234, debug=True)
