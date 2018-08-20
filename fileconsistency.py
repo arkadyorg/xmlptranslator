2cbaf1781c78f0d1c2a982555661b6a415e925a8
Arkady Bocharov <me@arkady.org>
2018-08-21 00:22:00 +0300

    added file_copy function to simplify code

--- fileconsistency.py
+++ fileconsistency.py
@@ -2,18 +2,19 @@ import shutil
 import os
 from dblogic import report_dir_translist, language_code_by_id
 
-#target_dir = "out/"
-#original_dir = "original/"
+target_dir = "out/"
+original_dir = "original/"
 #lang_id = 2
 def local_dir_naming(lang_id):
 	lang_code = language_code_by_id(lang_id)
 	raw_data = report_dir_translist(lang_id)
-
-	#print (raw_data)
 	replacements =[]
 	for report in raw_data:
 		end_dir = report['original_dir'].replace(report['original_name'], report['local_name'])
 		final_dir = end_dir.replace(original_dir, target_dir + lang_code + '/' )
-		#print(final_dir)
 		if not os.path.exists(final_dir):
 			os.makedirs(final_dir)
+
+
+def file_copy(source_dir, source_file, target_dir, target_file):
+ 	shutil.copyfile((os.path.join(source_dir, source_file)), (os.path.join(target_dir, target_file)))
