cdf65bcf6e12162a4cea3b26f4bdf601aa6e1e17
Arkady Bocharov <me@arkady.org>
2018-08-20 23:48:01 +0300

    created local_dir_naming function which creates all report with non null naming in local reports naming

--- fileconsistency.py
+++ fileconsistency.py
@@ -1,10 +1,19 @@
 import shutil
 import os
+from dblogic import report_dir_translist, language_code_by_id
 
-targetdir = "out/RU/01 Accounts/0101 Account balance/"
-targetdirs = [{'dir':'out/RU/01 Accounts/0101 Account balance/'},{'dir':'out/RU/02 Accounts2/0102 Account balance/'}]
-lang_id = 1
+#target_dir = "out/"
+#original_dir = "original/"
+#lang_id = 2
+def local_dir_naming(lang_id):
+	lang_code = language_code_by_id(lang_id)
+	raw_data = report_dir_translist(lang_id)
 
-for a in targetdirs:	
-	if not os.path.exists(a['dir']):
-	    os.makedirs(a['dir'])
+	#print (raw_data)
+	replacements =[]
+	for report in raw_data:
+		end_dir = report['original_dir'].replace(report['original_name'], report['local_name'])
+		final_dir = end_dir.replace(original_dir, target_dir + lang_code + '/' )
+		#print(final_dir)
+		if not os.path.exists(final_dir):
+			os.makedirs(final_dir)
