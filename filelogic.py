de134086706935bbe5c8b2a62c35b373e24d73eb
Arkady Bocharov <me@arkady.org>
2018-08-21 00:52:00 +0300

    added xdo_local_out_copy function

--- filelogic.py
+++ filelogic.py
@@ -1,7 +1,8 @@
 import os, fnmatch, collections 
-from dblogic import report_reindex, report_filepointer_select, template_reindex, parameters_reindex
+from dblogic import report_reindex, report_filepointer_select, template_reindex, parameters_reindex, report_dir_translist, language_code_by_id
 from xmlgetter import template_lister, parameters_lister
-
+from fileconsistency import file_copy
+import settings
 
 
 def report_reindex_igniter():
@@ -29,3 +30,12 @@ def parameters_reindex_igniter():
 		for parameters_entries in parameters_result:
 			parameters_reindex(parameters_entries['pl_report_id'], parameters_entries['pl_param_id'], parameters_entries['pl_param_lable'])
 
+def xdo_local_out_copy(lang_id):
+	target_dir = settings.target_dir
+	original_dir = settings.original_dir
+	lang_code = language_code_by_id(lang_id)
+	raw_data = report_dir_translist(lang_id)
+	for report in raw_data:
+		end_dir = report['original_dir'].replace(report['original_name'], report['local_name'])
+		final_dir = end_dir.replace(original_dir, target_dir + lang_code + '/' )
+		file_copy(report['original_dir'], (report['original_name']+'.xdo'), final_dir, (report['local_name']+'.xdo'))
