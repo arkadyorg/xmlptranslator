359208de25c29ce6a0ffae66c42bfe93640064ad
Arkady Bocharov <me@arkady.org>
2018-08-21 13:44:22 +0300

    test

--- xmlupdater.py
+++ xmlupdater.py
@@ -1,1 +1,13 @@
-import xml.etree.ElementTree as ET  
+import xml.etree.ElementTree as ET
+
+file_pointer = "0102 Балансы счетов детально.xdo"
+tree = ET.parse(file_pointer)
+root = tree.getroot()
+
+for template_param in root.iterfind('./{http://xmlns.oracle.com/oxp/xmlp}parameters//'):
+	label = template_param.attrib
+	if 'label' in label:
+		param_lable = label.get('label')
+		print (param_lable)
+	else:
+		pass
