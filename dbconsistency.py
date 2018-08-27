8c2d71d486e0ed81932530c1694b2386719e3807
Arkady Bocharov <me@arkady.org>
2018-08-27 22:48:09 +0300

    [dbconsistency.py]-pep8/ [firstrun.py]-installer

--- dbconsistency.py
+++ dbconsistency.py
@@ -2,23 +2,26 @@ from db import db_session, Reports, Templates, Parameters, Languages
 from dblogic import reports_name_strings_reindex, parameters_lable_strings_reindex, templates_lable_strings_reindex
 from datetime import datetime
 
+
 def report_naming():
-	ql = Languages.query.order_by(Languages.code).all()
-	qr = Reports.query.order_by(Reports.report_name).all()
-	qa = (db_session.query(Reports,Languages).all())
-	for repinf in qa:
-		reports_name_strings_reindex(repinf.Reports.id, repinf.Languages.id)
+    ql = Languages.query.order_by(Languages.code).all()
+    qr = Reports.query.order_by(Reports.report_name).all()
+    qa = (db_session.query(Reports,Languages).all())
+    for repinf in qa:
+        reports_name_strings_reindex(repinf.Reports.id, repinf.Languages.id)
+
 
 def parameters_lang_naming():
-	ql = Languages.query.order_by(Languages.code).all()
-	qp = Parameters.query.order_by(Parameters.parameter_id).all()
-	qa = (db_session.query(Parameters,Languages).all())
-	for paraminf in qa:
-		parameters_lable_strings_reindex(paraminf.Parameters.id, paraminf.Languages.id)
+    ql = Languages.query.order_by(Languages.code).all()
+    qp = Parameters.query.order_by(Parameters.parameter_id).all()
+    qa = (db_session.query(Parameters,Languages).all())
+    for paraminf in qa:
+        parameters_lable_strings_reindex(paraminf.Parameters.id, paraminf.Languages.id)
+
 
 def templates_lang_naming():
-	ql = Languages.query.order_by(Languages.code).all()
-	qt = Templates.query.order_by(Templates.id).all()
-	qa=db_session.query(Templates.id,Languages.id).filter(Templates.template_lang == Languages.code).all()
-	for templinf in qa:
-		templates_lable_strings_reindex(templinf[0], templinf[1])
+    ql = Languages.query.order_by(Languages.code).all()
+    qt = Templates.query.order_by(Templates.id).all()
+    qa=db_session.query(Templates.id,Languages.id).filter(Templates.template_lang == Languages.code).all()
+    for templinf in qa:
+        templates_lable_strings_reindex(templinf[0], templinf[1])
---
+++ firstrun.py
@@ -0,0 +1,18 @@
+# First run script
+import os
+from filelogic import report_reindex_igniter, template_reindex_igniter, parameters_reindex_igniter
+from dbconsistency import report_naming, parameters_lang_naming, templates_lang_naming
+
+
+# Create DB
+os.system("db.py")
+
+# Report base information collector
+report_reindex_igniter()
+template_reindex_igniter()
+parameters_reindex_igniter()
+
+# Generate data for translation data
+report_naming()
+parameters_lang_naming()
+templates_lang_naming()
