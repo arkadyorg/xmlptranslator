573e38c7fca44676ec14f3141c3b08a2e66facc7
Arkady Bocharov <me@arkady.org>
2018-09-02 17:34:18 +0300

    switched to dictionary_refresh function

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,10 @@
 from dblogic import dictionary_writer, dictionary_report_naming_miner, dictionary_report_parameters_miner
 
-lang = 2
-report_names_translist = dictionary_report_naming_miner(lang)
+def dictionary_refresh(lang_id):
+    report_names_translist = dictionary_report_naming_miner(lang_id)
+    report_parameters_translist = dictionary_report_parameters_miner(lang_id)
+    for item in report_names_translist:
+        dictionary_writer(item['lang_id'], item['datatype'], item['original'], item['translation'])
 
-report_parameters_translist = dictionary_report_parameters_miner(lang)
-
-for item in report_names_translist:
-	dictionary_writer(item['lang_id'], item['datatype'], item['original'], item['translation'])
-
-for item in report_parameters_translist:
-	dictionary_writer(item['lang_id'], item['datatype'], item['original'], item['translation'])
+    for item in report_parameters_translist:
+        dictionary_writer(item['lang_id'], item['datatype'], item['original'], item['translation'])
